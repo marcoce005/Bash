acd316897c8eae5e137718fc8aa9ecfdee6f029b
marcoce005 <marcoce005@gmail.com>
2024-12-07 11:56:52 +0100

    fix problem of not visible pop-up when an app is full-screen

--- custom_battery_threshold/battery_check_percentage.sh
+++ custom_battery_threshold/battery_check_percentage.sh
@@ -8,5 +8,5 @@ battery_status=`acpi -b | grep -P -o '[A-Za-z]+(?=,)'`;
 
 if [ $battery_level -gt 82 ] && [ $battery_status == "Charging" ]
 then
-	DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send "Unplug the charger" "Battery fully charge\nBattery level:\t$battery_level%" -a "Battery Pop-Up" -i battery
+	DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical "Unplug the charger" "Battery fully charge\nBattery level:\t$battery_level%" -a "Battery Pop-Up" -i battery
 fi
