36aea25c7648496c4afe40725789c0f302cdab5f
marcoce005 <marcoce005@gmail.com>
2025-05-14 22:17:53 +0200

    change in battery threshold: add alter for hyprland

--- custom_battery_threshold/battery_check_percentage.sh
+++ custom_battery_threshold/battery_check_percentage.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# script for bash shell and KDE desktop enviroment [tested on Garuda linux]
-
 battery_level=`acpi -b | grep "Battery 0" | grep -P -o '[0-9]+(?=%)'`;
 
 battery_status=`acpi -b | grep "Battery 0" | grep -P -o '[A-Za-z]+(?=,)'`;
 
+hyprland_active=`ps aux | grep hyprland | wc -l`;
+
+if [ $battery_level -le 25 ] && [ $battery_status == "Discharging" ] && [ $hyprland_active -eq 1 ]
+then
+	DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical "Plug the charger" "Low level battery\nBattery level:\t$battery_level%" -a "Battery Pop-Up" -i /usr/share/icons/BeautyLine/devices/scalable/xfpm-battery-000.svg
+fi
+
 if [ $battery_level -gt 82 ] && [ $battery_status == "Charging" ]
 then
-	DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical "Unplug the charger" "Battery fully charge\nBattery level:\t$battery_level%" -a "Battery Pop-Up" -i battery
+	DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical "Unplug the charger" "Battery fully charge\nBattery level:\t$battery_level%" -a "Battery Pop-Up" -i /usr/share/icons/BeautyLine/apps/scalable/battery.svg
 fi
