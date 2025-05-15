fe0c3a56afaa3b9f1d416cbf2614b46e78866547
marcoce005 <marcoce005@gmail.com>
2025-05-15 10:45:11 +0200

    change in battery threshold: fix problem of multi-battery selection

--- custom_battery_threshold/battery_check_percentage.sh
+++ custom_battery_threshold/battery_check_percentage.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-battery_level=`acpi -b | grep "Battery 0" | grep -P -o '[0-9]+(?=%)'`;
+battery_level=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E percentag | grep -P -o '[0-9]+(?=%)'`;
 
-battery_status=`acpi -b | grep "Battery 0" | grep -P -o '[A-Za-z]+(?=,)'`;
+battery_status=`cat /sys/class/power_supply/BAT0/status`;
 
 hyprland_active=`ps aux | grep hyprland | wc -l`;
 
 if [ $battery_level -le 25 ] && [ $battery_status == "Discharging" ] && [ $hyprland_active -eq 1 ]
 then
-	DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical "Plug the charger" "Low level battery\nBattery level:\t$battery_level%" -a "Battery Pop-Up" -i /usr/share/icons/BeautyLine/devices/scalable/xfpm-battery-000.svg
+	DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical "Plug the charger" "Low level battery\nBattery level:\t$battery_level%" -a "Battery Pop-Up" -i /usr/share/icons/BeautyLine/devices/scalable/xfpm-battery-000.svg;
 fi
 
-if [ $battery_level -gt 82 ] && [ $battery_status == "Charging" ]
+if [ $battery_level -gt 82 ]  && [ $battery_status == "Charging" ]
 then
-	DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical "Unplug the charger" "Battery fully charge\nBattery level:\t$battery_level%" -a "Battery Pop-Up" -i /usr/share/icons/BeautyLine/apps/scalable/battery.svg
+	DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical "Unplug the charger" "Battery fully charge\nBattery level:\t$battery_level%" -a "Battery Pop-Up" -i /usr/share/icons/BeautyLine/apps/scalable/battery.svg;
 fi
